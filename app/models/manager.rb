14f0ab22df01ba2456aca2ab4fc00a2bfd1ae9c3
Amirata Khodaparast <amiratak88@gmail.com>
2018-10-12 08:11:08 -0400

    Add Manager and Restaurant associations

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,2 +1,4 @@
 class Manager < ApplicationRecord
+	has_secure_password
+	belongs_to :restaurant
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,7 @@ User.destroy_all
 Restaurant.destroy_all
 Address.destroy_all
 UserAddress.destroy_all
+Manager.destroy_all
 
 user1 = User.create(first_name: "Amirata", last_name: "Khodaparast", username: "amirata", password: "123")
 
@@ -21,3 +22,5 @@ user1.addresses << address1
 user1.addresses << address2
 
 res1 = Restaurant.create(name: "Bougie Deli", address_id: address1.id, cuisine: "Chu know!", description: "We're awesome!")
+
+Manager.create(first_name: "Kevin", last_name: "Rivera", username: "kevin", password: "123", restaurant_id: res1.id)
