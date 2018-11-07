0fe9abcf251fcfcc75631493522cbf139789129d
Amirata Khodaparast <amiratak88@gmail.com>
2018-11-07 09:07:52 -0500

    Add 'status' attribute to 'orders' table

--- db/migrate/20181011150314_create_orders.rb
+++ db/migrate/20181011150314_create_orders.rb
@@ -1,10 +1,11 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
-  def change
-    create_table :orders do |t|
-      t.integer :user_id
-      t.integer :restaurant_id
+	def change
+		create_table :orders do |t|
+			t.integer :user_id
+			t.integer :restaurant_id
+			t.string :status, default: "initiated"
 
-      t.timestamps
-    end
-  end
+			t.timestamps
+		end
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2018_10_11_172720) do
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
     t.integer "restaurant_id"
+    t.string "status", default: "initiated"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
