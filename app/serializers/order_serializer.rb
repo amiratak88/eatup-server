3ce57cde274a9f44ba30b2ec2ac05cbc517a23a8
Amirata Khodaparast <amiratak88@gmail.com>
2018-11-09 09:49:56 -0500

    include 'restaurant' in 'OrderSerializer'

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,6 +1,6 @@
 class OrderSerializer < ActiveModel::Serializer
 	attributes :id, :user_id, :restaurant_id, :status
 	# belongs_to :user
-	# belongs_to :restaurant
+	belongs_to :restaurant
 	has_many :order_items
 end
