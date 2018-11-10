70cda2e2663dcd2c393a5d7f3e41d56eacf48c4d
Amirata Khodaparast <amiratak88@gmail.com>
2018-11-09 22:32:31 -0500

    Add ':updated_at to 'OrderSerializer'

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,5 @@
 class OrderSerializer < ActiveModel::Serializer
-	attributes :id, :user_id, :restaurant_id, :status
+	attributes :id, :user_id, :restaurant_id, :status, :updated_at
 	# belongs_to :user
 	belongs_to :restaurant
 	has_many :order_items
