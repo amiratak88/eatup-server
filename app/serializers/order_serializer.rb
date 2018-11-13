0b929fea9f4b5edc8ebfbe1a8d35bd6f747e494a
Amirata Khodaparast <amiratak88@gmail.com>
2018-11-13 18:24:44 -0500

    Use scope in order to show only the necessary data for manager

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,7 @@
 class OrderSerializer < ActiveModel::Serializer
-	attributes :id, :user_id, :restaurant_id, :status, :updated_at
+	attributes :id, :user_id, :status, :updated_at
+	attribute :restaurant_id, if: -> {!is_manager?}
+	attribute :user_id, if: -> {!is_manager?}
 	belongs_to :user, if: -> {is_manager?}
 	belongs_to :restaurant, if: -> {!is_manager?}
 	has_many :order_items
