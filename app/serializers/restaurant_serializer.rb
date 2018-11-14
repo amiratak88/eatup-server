e554a15462c8f64244b416dea03db4fa4555a727
Amirata Khodaparast <amiratak88@gmail.com>
2018-11-13 22:55:41 -0500

    Add :phone_number to both restaurant and order serializers

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,5 +1,5 @@
 class RestaurantSerializer < ActiveModel::Serializer
-	attributes :id, :name, :cuisine, :description, :imgURL
+	attributes :id, :name, :cuisine, :description, :imgURL, :phone_number
 	belongs_to :address
 	# has_one :manager
 	has_many :items
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-	attributes :id, :first_name, :last_name, :username
+	attributes :id, :first_name, :last_name, :username, :phone_number
 	has_many :addresses
 	has_many :favorited_restaurants, if: -> {is_user?}
 	has_many :ordered_restaurants, if: -> {is_user?}
